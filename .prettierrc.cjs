0ce44361403c12a2ee2592b6a1cc790af1fe2387
seung <seung.dev@gmail.com>
2025-01-01 13:59:14 +0900

    feat: Configuring Prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+	printWidth: 128, // 80(default)
+	tabWidth: 4, // 2(default)
+	useTabs: true, // false(default)
+	semi: true, // true(default)
+	singleQuote: false, // false(default)
+	trailingComma: "all", // es5(default), none, all
+	bracketSpacing: true, // true(default)
+	bracketSameLine: false, // false(default)
+	arrowParens: "always", // always(default), avoid
+	proseWrap: "preserve", // preserve(default), always, never
+	endOfLine: "lf", // lf(default), crlf, cr, auto
+	singleAttributePerLine: true, // false(default)
+	plugins: ["@trivago/prettier-plugin-sort-imports"],
+	importOrder: [
+		"^[./].*\\.(svg|png|jpeg|jpg|gif|webp)$",
+		"^[./].*\\.(c|le|sc)ss$",
+		"^vite",
+		"^vite(.*)$",
+		"^@vite(.*)$",
+		"^react$",
+		"^react-dom$",
+		"^react-router-dom$",
+		"^react(.*)$",
+		"<BUILTIN_MODULES>",
+		"<THIRD_PARTY_MODULES>",
+		"^@refinedev(.*)$",
+		"^@mui(.*)$",
+		"^@/(!app)(.*)$",
+		"^@/app(.*)$",
+		"^[./]",
+	],
+	importOrderSeparation: true,
+	importOrderSortSpecifiers: true,
+	importOrderCaseInsensitive: true,
+};
--- HISTORY.md
+++ HISTORY.md
@@ -1,11 +1,64 @@
 # History
 
-### Create Vite App
+##### Dependencies
+
+- prettier
+- @trivago/prettier-plugin-sort-imports
+
+##### Dev Dependencies
+
+### 0-0. Creating a Vite Application
 
 ```cmd
-npm create vite@latest .
+npm create vite@latest . -- --template react-swc-ts
 ```
 
 ```cmd
 npm install
+```
+
+### 0-1. Configuring Prettier
+
+```cmd
+npm install --save-dev prettier @trivago/prettier-plugin-sort-imports
+```
+
+Prettier 설정
+
+```cmd
+echo. > ".prettierrc.cjs"
+```
+
+```cmd
+echo. > ".prettierignore"
+```
+
+Visual Studio Code 설정
+
+```cmd
+mkdir .vscode
+```
+
+```cmd
+echo. > .vscode\settings.json
+```
+
+Git 설정
+
+```cmd
+notepad .gitignore
+```
+
+```cmd
+echo. > .gitattributes
+```
+
+Prettier Command 설정
+
+```cmd
+notepad package.json
+```
+
+```cmd
+npm run prettier
 ```
